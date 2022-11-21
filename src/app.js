08af782b9cf976787fe56f3a795ff80bd0f5ad73
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2022-11-21 14:13:22 -0300

    static files

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 import express from "express";
-import {create} from 'express-handlebars';
-import indexRoutes from './routes/index.routes';
-import path from 'path';
+import { create } from "express-handlebars";
+import indexRoutes from "./routes/index.routes";
+import path from "path";
 import morgan from "morgan";
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 const exphbs = create({
-    layoutsDir: path.join(__dirname, 'layouts'),
-    partialsDir: path.join(app.get("views"), 'partials'),
-    extname: ".hbs",
-    defaultLayout: 'main'
+  layoutsDir: path.join(__dirname, "layouts"),
+  partialsDir: path.join(app.get("views"), "partials"),
+  extname: ".hbs",
+  defaultLayout: "main",
 });
 
 app.engine(".hbs", exphbs.engine);
-app.set("view engine", ".hbs")
+app.set("view engine", ".hbs");
 
 //midlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false}))
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 app.use(indexRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname, "public")));
 
 export default app;
