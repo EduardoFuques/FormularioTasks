52de0f523411b7cd62df29cff933e9025272e581
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2022-11-21 13:40:35 -0300

    bootstrap

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://julio:Julio2022@172.27.2.249:27017/appj';
 export const PORT = process.env.PORT || 3000;
