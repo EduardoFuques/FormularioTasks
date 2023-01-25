bf10bafcf535a3fda6b07ae4d77e72401efce4dd
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2023-01-25 16:48:10 -0300

    a medio camino, solucionando bugs

--- src/helpers/multer.js
+++ src/helpers/multer.js
@@ -10,27 +10,40 @@ function formatoFecha(fecha, formato) {
     yyyy: fecha.getFullYear(),
     hh: fecha.getHours(),
     mi: fecha.getMinutes(),
-    ss: fecha.getSeconds()
+    ss: fecha.getSeconds(),
   };
 
   return formato.replace(/dd|mm|yyyy|hh|mi|ss/gi, (matched) => map[matched]);
 }
 const hoyformateado = formatoFecha(hoy, "yyyymmddhhmiss-");
 
-const storage = multer.diskStorage({
+const storageCV = multer.diskStorage({
   destination: path.join(__dirname, "..", "files"),
   filename: (req, file, cb) => {
     cb(
       null,
       hoyformateado +
-      req.user.usuario.toString() +
+        req.user.usuario.toString() +
         path.extname(file.originalname).toLowerCase()
     );
   },
 });
 
-export const fileUpload = multer({
-  storage: storage,
+// const storageDNI = multer.diskStorage({
+//   destination: path.join(__dirname, "..", "files"),
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       hoyformateado +
+//         req.user.usuario.toString() +
+//         +"-DNI" +
+//         path.extname(file.originalname).toLowerCase()
+//     );
+//   },
+// });
+
+export const cvUpload = multer({
+  storage: storageCV,
   limits: {
     fileSize: FILE_SIZE,
   },
@@ -46,3 +59,19 @@ export const fileUpload = multer({
   dest: path.join(__dirname, "..", "files"),
 }).single("cvFile");
 
+// export const dniUpload = multer({
+//   storage: storageDNI,
+//   limits: {
+//     fileSize: FILE_SIZE,
+//   },
+//   fileFilter: (req, file, cb) => {
+//     const filetypes = /pdf/;
+//     const mimetype = filetypes.test(file.mimetype);
+//     const extname = filetypes.test(path.extname(file.originalname));
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     }
+//     cb("Error: tipo de archivo no soportado");
+//   },
+//   dest: path.join(__dirname, "..", "files"),
+// }).single("dniFile");
